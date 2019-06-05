8fad8ff5055bbed332295919fbcebda881bc2c5c
Alex7Ander <alex.ander.1991@mail.ru>
2019-06-05 23:35:15 +0300

    Initial commit: db class created

---
+++ PhoneBook/DataBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace PhoneBook
+{
+    class DataBase
+    {
+        private static DataBase instanceDB;
+        private OleDbConnection aConnection;
+        private OleDbCommand aCommand;
+        private OleDbDataReader aReader;
+        private string connetionString;
+        private string lastCommand { get; }
+
+        private DataBase(string filePath){
+            this.connetionString = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source="+filePath;
+            this.aConnection = new OleDbConnection(this.connetionString);
+            this.aCommand = new OleDbCommand("", this.aConnection);
+        }
+
+        public int select(string sqlQuery, string[] result){
+
+            this.aCommand.CommandText = sqlQuery;
+            try{
+                this.aConnection.Open();
+                this.aReader = aCommand.ExecuteReader();
+                int i = 0;
+                while (aReader.Read()) {
+                    result[i] = aReader.GetString(i);
+                    ++i;
+                }
+                this.aConnection.Close();
+                return i;
+            }
+            catch(OleDbException exp){
+                return -1;
+            }
+        }
+
+        static DataBase getInstanceDB(string filePath){
+            if (instanceDB == null)
+                instanceDB = new DataBase(filePath);
+            return instanceDB;
+        }
+    }
+}
