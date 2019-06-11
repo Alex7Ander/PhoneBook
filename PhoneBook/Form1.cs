1af171a83754164fcd69ec62f6a63020f20495f7
Alex7Ander <alex.ander.1991@mail.ru>
2019-06-11 22:39:24 +0300

    Photo showing

--- PhoneBook/Form1.cs
+++ PhoneBook/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,13 @@ namespace PhoneBook
 {
     public partial class MainForm : Form
     {
+        private string currentPhotoPath;
+
         public MainForm()
         {
+            
             InitializeComponent();
+            this.photoPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
         }
 
         private void MainForm_Shown(object sender, EventArgs e)
@@ -50,6 +55,22 @@ namespace PhoneBook
             }
         }
 
+        private void clearGUI()
+        {
+            
+            this.photoPictureBox.Dispose();
+            this.photoPictureBox.Image = null;
+
+            this.nameTextBox.Text = "";
+            this.surNameTextBox.Text = "";
+            this.MidNameTextBox.Text = "";
+            this.mobilePhoneTextBox.Text = "";
+            this.workPhoneTextBox.Text = "";
+            this.homePhoneTextBox.Text = "";
+            this.emailTextBox.Text = "";
+            this.addrTextBox.Text = "";
+        }
+
         private void personsListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             string personItem = this.personsListBox.SelectedItem.ToString();
@@ -70,6 +91,10 @@ namespace PhoneBook
             this.workPhoneTextBox.Text = anyPerson.WorkPhone;
             this.emailTextBox.Text = anyPerson.Email;
             this.addrTextBox.Text = anyPerson.Addres;
+            this.currentPhotoPath = anyPerson.PhotoPath;
+            if (this.currentPhotoPath == null)
+                this.currentPhotoPath = CurrentDirrectoryReturner.getDirrectory() + "\\data\\noPhotoAvailable.jpg";
+            this.photoPictureBox.Image = Image.FromFile(this.currentPhotoPath);
         }
 
         private void saveButton_Click(object sender, EventArgs e)
@@ -79,7 +104,13 @@ namespace PhoneBook
             string _MiddleName = this.MidNameTextBox.Text;
             string _SurName = this.surNameTextBox.Text;
             Person anyPerson = new Person(_GroupName, _Name, _MiddleName, _SurName, true);
-            int res = anyPerson.update();
+            //int res = anyPerson.update();
+            String newPhotoPath = CurrentDirrectoryReturner.getDirrectory() + "\\data\\Photos\\"+
+                anyPerson.Groupname+"_"+ anyPerson.Surname+"_"+anyPerson.Name+"_"+anyPerson.MiddleName +".jpg";
+            if (!anyPerson.PhotoPath.Equals(newPhotoPath)) {
+                File.Copy(currentPhotoPath, newPhotoPath);
+            }
+                       
         }
 
         private void addButton_Click(object sender, EventArgs e)
@@ -95,7 +126,8 @@ namespace PhoneBook
                 String _WorkPhone = this.workPhoneTextBox.Text;
                 String _Email = this.emailTextBox.Text;
                 String _Addres = this.addrTextBox.Text;
-                String _PhotoPath = "";
+                String _PhotoPath = CurrentDirrectoryReturner.getDirrectory() + "\\data\\Photos\\" +
+                _GroupName + "_" + _SurName + "_" + _Name + "_" + _MiddleName + ".jpg"; ;
                 Person newPerson = new Person(_GroupName, _Name, _MiddleName, _SurName, _MobilePhone,
                    _HomePhone, _WorkPhone, _Email, _Addres, _PhotoPath);
                 int res = newPerson.save();
@@ -103,6 +135,7 @@ namespace PhoneBook
                 {
                     this.updateGroupsList();
                     this.updatePersonsList();
+                    File.Copy(this.currentPhotoPath, _PhotoPath, true);
                     MessageBox.Show("Успешное сохранение!", "Успешно!");
                 }
                 else
@@ -118,20 +151,27 @@ namespace PhoneBook
 
         private void photoButton_Click(object sender, EventArgs e)
         {
-            //
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.currentPhotoPath = openFileDialog1.FileName;
+                this.photoPictureBox.Image = Image.FromFile(this.currentPhotoPath);
+            }
         }
 
         private void deleteButton_Click(object sender, EventArgs e)
         {
+            string _GroupName = this.groupsComboBox.SelectedItem.ToString();
+            string _Name = this.nameTextBox.Text;
+            string _MiddleName = this.MidNameTextBox.Text;
+            string _SurName = this.surNameTextBox.Text;
+            Person anyPerson = new Person(_GroupName, _Name, _MiddleName, _SurName, true);
             try
             {
-                string _GroupName = this.groupsComboBox.SelectedItem.ToString();
-                string _Name = this.nameTextBox.Text;
-                string _MiddleName = this.MidNameTextBox.Text;
-                string _SurName = this.surNameTextBox.Text;
-                Person anyPerson = new Person(_GroupName, _Name, _MiddleName, _SurName, true);
                 int res = anyPerson.delete();
                 if (res == 0){
+                    this.clearGUI();
                     this.updatePersonsList();
                     MessageBox.Show("Успешное удаление!", "Успешно!");
                 }
@@ -142,6 +182,10 @@ namespace PhoneBook
             catch (Exception exp){
                 MessageBox.Show("Ошибка! Удаление не было завершено! Попробуйте повторить попытку!", "Ошибка!");
             }
+
+            this.photoPictureBox.Dispose();
+            if (anyPerson.PhotoPath!=null)
+                File.Delete(anyPerson.PhotoPath);
         }
     }
 }
